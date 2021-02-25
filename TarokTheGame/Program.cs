ad7620bfdf397a893256040c3a282f3f7cbe69c4
Joachim Grubbe Frank <joac1067@stud.kea.dk>
2021-02-25 10:21:32 +0100

    Add project files.

---
+++ TarokTheGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TarokTheGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
